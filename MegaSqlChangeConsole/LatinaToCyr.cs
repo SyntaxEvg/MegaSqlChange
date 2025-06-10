e7a08f8f97c32ad49b36e973ed8580b0fc89adfa
xakzone <jjw2013@yandex.ru>
2025-06-10 22:54:17 +0300

    португальский

--- MegaSqlChangeConsole/LatinaToCyr.cs
+++ MegaSqlChangeConsole/LatinaToCyr.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-//ÀÂÆÇÉÈÊËÎÏÔŒÙÛÜŸàâæçéèêëîïôœùûüÿÄÖÜẞßäöüÁÉÍÑÓÚÜáéíñóúüĄĆĘŁŃÓŚŹŻąćęłńóśźżÇĞİIÖŞÜçğıiöşü
+//ÀÂÃÆÇÉÈÊËÎÏÔŒÙÛÜŸàâãæçéèêëîïôœùûüÿÄÖÜẞßäöüÁÉÍÑÓÔÕÚÜáéíñóôõúüĄĆĘŁŃÓŚŹŻąćęłńóśźżÇĞİIÖŞÜçğıiöşü
 
 //public class ConsoleLogger : ILogger
 //{
